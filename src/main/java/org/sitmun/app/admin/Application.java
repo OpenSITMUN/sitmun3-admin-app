ce91010268b5849c5b7cdfe2143ce50e4b74b0a4
rrioja <rodolfo@geoslab.com>
2018-04-03 18:38:00 +0200

    primera versión sprint 1

--- src/main/java/org/sitmun/app/admin/Application.java
+++ src/main/java/org/sitmun/app/admin/Application.java
@@ -2,7 +2,15 @@ package org.sitmun.app.admin;
 
 import org.sitmun.plugin.core.annotation.SitmunApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Import(SpringDataRestConfiguration.class)
 @SitmunApplication
 public class Application {
     public static void main(String[] args) {
