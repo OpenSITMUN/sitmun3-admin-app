63f490c105a5aa3ed61ffdcfb17f87bea9d975a7
rrioja <rodolfo@geoslab.com>
2018-04-05 17:08:49 +0200

    Corrijo error index.html, añado icono

---
+++ src/test/java/org/sitmun/app/admin/app/ApplicationTests.java
@@ -0,0 +1,19 @@
+package org.sitmun.app.admin.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sitmun.app.admin.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        String[] args = new String[] {};
+        Application.main(args);
+    }
+}
--- src/test/java/org/sitmun/app/admin/app/CorePluginIntegrationTest.java
+++ src/test/java/org/sitmun/app/admin/app/CorePluginIntegrationTest.java
@@ -1,33 +1,35 @@
 package org.sitmun.app.admin.app;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class CorePluginIntegrationTest {
     @LocalServerPort
     private int port;
     
     @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    public void configjsIsPresent() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/app/config.js", String.class))
-                .contains("requirejs.config");
-    }
+    private MockMvc mvc;
     
-//    @Test
-//    public void jsIsPresent() throws Exception {
-//        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/app/config.js", String.class))
-//                .contains("sitmun3-plugin-core");
-//    }
+
+    
+    @Test
+    public void testCoreAPI() throws Exception {
+        mvc.perform(get("/api/usuarios")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                ;
+    }
 }
