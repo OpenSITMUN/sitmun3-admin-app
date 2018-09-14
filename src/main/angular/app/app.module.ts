03b8dfaf860fe4e043f14c5218e47431ec1a6177
rrioja <rodolfo@geoslab.com>
2018-09-14 12:41:28 +0200

    Update with new menu options

--- src/main/angular/app/app.module.ts
+++ src/main/angular/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomeComponent } from './home/home.component';
 import {HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import { SitmunPluginCoreModule, TerritoryListComponent, TerritoryEditComponent, TerritoryTypeListComponent, TerritoryTypeEditComponent, RoleListComponent, RoleEditComponent, UserListComponent, UserEditComponent,AuthInterceptor,AuthExpiredInterceptor, UserChangePasswordComponent,LoginComponent,LoginService,AccountEditComponent,AccountChangePasswordComponent,HasAnyAuthorityDirective,HasAnyAuthorityOnTerritoryDirective } from 'sitmun-plugin-core';
+import { SitmunPluginCoreModule, TerritoryListComponent, TerritoryEditComponent, TerritoryTypeListComponent, TerritoryTypeEditComponent, RoleListComponent, RoleEditComponent, UserListComponent, UserEditComponent,AuthInterceptor,AuthExpiredInterceptor, UserChangePasswordComponent,LoginComponent,LoginService,AccountEditComponent,AccountChangePasswordComponent,HasAnyAuthorityDirective,HasAnyAuthorityOnTerritoryDirective,ConnectionListComponent,ConnectionEditComponent,TaskListComponent,TaskEditComponent,TaskTypeListComponent,TaskTypeEditComponent,TaskGroupListComponent,TaskGroupEditComponent,ServiceListComponent,ServiceEditComponent,CartographyEditComponent,CartographyListComponent,CartographyGroupEditComponent,CartographyGroupListComponent,BackgroundListComponent, BackgroundEditComponent,TreeListComponent,TreeEditComponent,ApplicationListComponent,ApplicationEditComponent  } from 'sitmun-plugin-core';
 
 const appRoutes: Routes = [
     {
@@ -83,7 +83,118 @@ const appRoutes: Routes = [
     {
         path: 'user-change-password/:id',
         component: UserChangePasswordComponent
+    },    {
+        path: 'connection-list',
+        component: ConnectionListComponent
+    },
+    {
+        path: 'connection-add',
+        component: ConnectionEditComponent
+    },
+    {
+        path: 'connection-edit/:id',
+        component: ConnectionEditComponent
+    },  {
+        path: 'task-list',
+        component: TaskListComponent
+    },
+    {
+        path: 'task-add',
+        component: TaskEditComponent
+    },
+    {
+        path: 'task-edit/:id',
+        component: TaskEditComponent
+    },  {
+        path: 'task-group-list',
+        component: TaskGroupListComponent
+    },
+    {
+        path: 'task-group-add',
+        component: TaskGroupEditComponent
+    },
+    {
+        path: 'task-group-edit/:id',
+        component: TaskGroupEditComponent
+    },  {
+        path: 'task-type-list',
+        component: TaskTypeListComponent
+    },
+    {
+        path: 'task-type-add',
+        component: TaskTypeEditComponent
+    },
+    {
+        path: 'task-type-edit/:id',
+        component: TaskTypeEditComponent
+    },  {
+        path: 'service-list',
+        component: ServiceListComponent
+    },
+    {
+        path: 'service-add',
+        component: ServiceEditComponent
+    },
+    {
+        path: 'service-edit/:id',
+        component: ServiceEditComponent
+    },  {
+        path: 'cartography-list',
+        component: CartographyListComponent
+    },
+    {
+        path: 'cartography-add',
+        component: CartographyEditComponent
+    },
+    {
+        path: 'cartography-edit/:id',
+        component: CartographyEditComponent
+    },  {
+        path: 'cartography-group-list',
+        component: CartographyGroupListComponent
+    },
+    {
+        path: 'cartography-group-add',
+        component: CartographyGroupEditComponent
+    },
+    {
+        path: 'cartography-group-edit/:id',
+        component: CartographyGroupEditComponent
+    },  {
+        path: 'background-list', 
+        component: BackgroundListComponent
+    },
+    {
+        path: 'background-add',
+        component: BackgroundEditComponent
+    },
+    {
+        path: 'background-edit/:id',
+        component: BackgroundEditComponent
+    },  {
+        path: 'tree-list', 
+        component: TreeListComponent
+    },
+    {
+        path: 'tree-add',
+        component: TreeEditComponent
+    },
+    {
+        path: 'tree-edit/:id',
+        component: TreeEditComponent
+    },  {
+        path: 'application-list', 
+        component: ApplicationListComponent
+    },
+    {
+        path: 'application-add',
+        component: ApplicationEditComponent
+    },
+    {
+        path: 'application-edit/:id',
+        component: ApplicationEditComponent
     }
+
 ];
 
 @NgModule({
