befdd80e897e460e354631f81f500bf16a1fccc4
ivans155 <ivans@geoslab.com>
2019-01-21 11:31:06 +0100

    Add configuration to enable the generation of deployable war files

--- src/main/java/org/sitmun/app/admin/Application.java
+++ src/main/java/org/sitmun/app/admin/Application.java
@@ -7,10 +7,19 @@ import org.springframework.context.annotation.Import;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 @EnableSwagger2
 @Import(SpringDataRestConfiguration.class)
 @SitmunApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
